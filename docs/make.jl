cf81504e993766adb952a78980c880540eb97582
MartinMikkelsen <MartinMikkelsen@protonmail.com>
2025-09-08 21:18:07 +0200

    updated wording and formatting

--- docs/make.jl
+++ docs/make.jl
@@ -4,24 +4,24 @@ using Documenter
 DocMeta.setdocmeta!(MetropolisAlgorithm, :DocTestSetup, :(using MetropolisAlgorithm); recursive = true)
 
 makedocs(;
-  modules = [MetropolisAlgorithm],
-  authors = "Shuhei Ohno",
-  sitename = "MetropolisAlgorithm.jl",
-  format = Documenter.HTML(;
-    canonical = "https://JuliaFewBody.github.io/MetropolisAlgorithm.jl",
-    edit_link = "main",
-    assets = String[
-      "./assets/logo.ico",
+    modules = [MetropolisAlgorithm],
+    authors = "Shuhei Ohno",
+    sitename = "MetropolisAlgorithm.jl",
+    format = Documenter.HTML(;
+        canonical = "https://JuliaFewBody.github.io/MetropolisAlgorithm.jl",
+        edit_link = "main",
+        assets = String[
+            "./assets/logo.ico",
+        ],
+    ),
+    pages = [
+        "Home" => "index.md",
+        "Examples" => "examples.md",
+        "API reference" => "API.md",
     ],
-  ),
-  pages = [
-    "Home"          => "index.md",
-    "Examples"      => "examples.md",
-    "API reference" => "API.md",
-  ],
 )
 
 deploydocs(;
-  repo = "github.com/JuliaFewBody/MetropolisAlgorithm.jl",
-  devbranch = "main",
+    repo = "github.com/JuliaFewBody/MetropolisAlgorithm.jl",
+    devbranch = "main",
 )
